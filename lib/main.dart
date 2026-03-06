5367d0506389e834fa0258af586e115eb2dcae9c
Islombek <matkaimovislambek@gmail.com>
2026-03-07 00:32:27 +0500

    homework#1 done

--- lib/main.dart
+++ lib/main.dart
@@ -13,26 +13,38 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Column(
+          child: Row(
             crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Container(
-                width: 300,
+                width: 100,
                 height: 100,
-                color: Colors.white,
+                color: Colors.red,
                 child: Text('Container 1'),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                    child: Text('Container 2'),
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.green[300],
+                    child: Text('Container 3'),
+                  ),
+                ],
               ),
               Container(
                 width: 100,
                 height: 100,
                 color: Colors.blue,
-                child: Text('Container 2'),
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.red,
-                child: Text('Container 3'),
+                child: Text('Container 4'),
               ),
             ],
           ),
